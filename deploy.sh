b12830b123cd54d7f9c146718c3336d7227bf647
Николай Морока <nikolaymoroka@MacBook-Pro-Nikolaj.local>
2022-10-21 10:21:16 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 
-set -e
+# set -e
 
 npm run build
 
 cd dist
 
-git init
-git add -A 
+# git init 
+git add .
 git commit -m "New Deployment"
+# git push -u origin gh-pages
 git push -f https://github.com/NikolayMoroka/MyWebSite_2.0.git master:gh-pages
 
 cd -
